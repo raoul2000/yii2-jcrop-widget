c27ae4c6d08c28c37967288b61012212d7eb7583
raoul <raoul_boulard@hotmail.com>
2014-07-30 15:01:46 +0200

    Apply Yii2 coding Standard

--- JCropAsset.php
+++ JCropAsset.php
@@ -1,23 +1,30 @@
 <?php
 namespace raoul2000\jcrop;
+
 use yii\web\AssetBundle;
 
 /**
+ *
  * @author Raoul <raoul.boulard@gmail.com>
  */
 class JCropAsset extends AssetBundle
 {
+
 	public $css = [
 		'css/jquery.Jcrop.css'
 	];
+
 	public $js = [
 		'js/jquery.Jcrop.js'
 	];
+
 	public $depends = [
-		'yii\web\JqueryAsset',
+		'yii\web\JqueryAsset'
 	];
-	public function init() {
-		$this->sourcePath = __DIR__.'/assets';
+
+	public function init()
+	{
+		$this->sourcePath = __DIR__ . '/assets';
 		return parent::init();
-	}	
+	}
 }
